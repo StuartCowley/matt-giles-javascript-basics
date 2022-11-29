4a56b823e4b50d6e823fe6edd30e23252df3ba46
Matthew Giles <matty92g@hotmail.co.uk>
2022-11-29 23:29:56 +0000

    First days changes commit

--- src/booleans.js
+++ src/booleans.js
@@ -1,37 +1,42 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+  return a && b;
 };
 
 function either(a, b) {
-  // your code here
+  return a || b;
 };
 
 function none(a, b) {
-  // your code here
+  return !(a || b);
 };
 
 function one(a, b) {
-  // your code here
+  return !(a===b);
 };
 
 function truthiness(a) {
-  // your code here
+  if (a) {
+    return true;
+  }
+  else {
+    return false;
+  }
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a===b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,52 @@
 function add (a, b) {
-  // your code here
+  return a + b;
 }
 
 function subtract (a, b) {
-  // your code here
+  return a - b;
 }
 
 function multiply (a, b) {
-  // your code here
+  return a * b;
 }
 
 function divide (a, b) {
-  // your code here
+  return a / b;
 }
 
 function power (a, b) {
-  // your code here
+  return a ** b;
 }
 
 function round (a) {
-  // your code here
+  return Math.round(a);
 }
 
 function roundUp (a) {
-  // your code here
+  return Math.ceil(a);
 }
 
 function roundDown (a) {
-  // your code here
+  return Math.floor(a);
 }
 
 function absolute (a) {
-  // your code here
+  return Math.abs(a);
 }
 
 function quotient (a, b) {
-  // your code here
+  let solution;
+  if (a > 0 && b > 0) {
+    solution = Math.floor(a / b);
+  }
+  else {
+    solution = Math.ceil(a / b);
+  }
+  return solution;
 }
 
 function remainder (a, b) {
-  // your code here
+  return a % b;
 }
 
 module.exports = {
